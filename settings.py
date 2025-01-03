6ee86d550c97d46e8dc6f462892d27ae3bef5c59
Justin Schmitz <justin.schmitz97@gmail.com>
2025-01-03 13:29:01 +0100

    .

--- settings.py
+++
@@ -1,6 +0,0 @@
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-}
-
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
